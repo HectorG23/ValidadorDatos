e7a2da06c01e897aeb2b4c216d264d301ec5148e
HectorG23 <hdgc2006@gmail.com>
2025-04-04 11:18:49 -0500

    cambio a gunicorn

--- startup.sh
+++ startup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-# Instala el controlador ODBC 18 para SQL Server
+# Instala ODBC Driver 18
+apt-get update && apt-get install -y curl gnupg
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 apt-get update
-ACCEPT_EULA=Y apt-get install -y msodbcsql18
-apt-get install -y unixodbc-dev
+ACCEPT_EULA=Y apt-get install -y msodbcsql18 unixodbc-dev
 
-# Ejecuta tu aplicación
-gunicorn wsgi:app
+# Instala dependencias de Python
+pip install -r requirements.txt
+
+# Inicia la app con Gunicorn (apunta al módulo Flask)
+gunicorn --bind 0.0.0.0:$PORT app.__init__:app
