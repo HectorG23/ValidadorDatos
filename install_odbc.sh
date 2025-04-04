8309ef884d60749bc7f84dc8b6f92c14c8d8721e
HectorG23 <hdgc2006@gmail.com>
2025-04-04 10:36:42 -0500

    se creo un script para intallar el driver de la bd en render

---
+++ install_odbc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Actualiza los paquetes e instala dependencias necesarias
+apt-get update && apt-get install -y \
+    curl \
+    gnupg \
+    unixodbc \
+    unixodbc-dev \
+    libssl-dev
+
+# Agrega la clave y el repositorio de Microsoft
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list
+
+# Actualiza los paquetes e instala el controlador ODBC 18
+apt-get update && ACCEPT_EULA=Y apt-get install -y msodbcsql18
+
+# Limpia los archivos temporales
+apt-get clean && rm -rf /var/lib/apt/lists/*
