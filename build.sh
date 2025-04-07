03b1c6dbc74b7fc85846db4bbe2391672865400c
HectorG23 <hdgc2006@gmail.com>
2025-04-07 15:41:43 -0500

    modificacion archivo .sh

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
+# build.sh
+# Descargar el paquete .deb del driver
+curl -O https://packages.microsoft.com/ubuntu/20.04/prod/pool/main/m/msodbcsql18/msodbcsql18_18.3.2.1-1_amd64.deb
 
-# Instalar el ODBC Driver 18 para SQL Server (sin sudo)
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
-apt-get update
-ACCEPT_EULA=Y apt-get install -y msodbcsql18
+# Instalar localmente sin privilegios root
+dpkg -x msodbcsql18_18.3.2.1-1_amd64.deb ~/.local/
+
+# Configurar variables de entorno
+echo 'export LD_LIBRARY_PATH=~/.local/opt/microsoft/msodbcsql18/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
+echo 'export PATH=~/.local/opt/microsoft/msodbcsql18/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
