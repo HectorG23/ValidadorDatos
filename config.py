ed7cedcfda98cd9c7e0652619e0aba3336e19a16
HectorG23 <hdgc2006@gmail.com>
2025-04-08 11:55:44 -0500

    modificacion del config.py

--- config.py
+++ config.py
@@ -1,18 +1,20 @@
-import os
+import json
+
+# Leer las credenciales desde un archivo externo
+with open('secrets.json') as f:
+    secrets = json.load(f)
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'clave_predeterminada')
+    DB_CONFIG = {
+        'server': 'sqls-ur-datamining-dev.database.windows.net',
+        'database': 'DB_ValidadorArchivos',
+        'username': secrets['db_user'],
+        'password': secrets['db_password'],
+        'driver': 'ODBC Driver 18 for SQL Server',
+        'authentication': 'ActiveDirectoryPassword',
+    }
 
-    DB_SERVER = os.environ.get('DATABASE_SERVER')
-    DB_NAME = os.environ.get('DATABASE_NAME')
-    DB_USER = os.environ.get('DATABASE_USER')
-    DB_PASSWORD = os.environ.get('DATABASE_PASSWORD')
-    DB_DRIVER = os.environ.get('DATABASE_DRIVER')
-
-    SQLALCHEMY_DATABASE_URI = (
-        f"mssql+pyodbc://{DB_USER}:{DB_PASSWORD}@{DB_SERVER}:1433/{DB_NAME}"
-        f"?driver={DB_DRIVER.replace(' ', '+')}"
-        f"&authentication=ActiveDirectoryPassword"
-    )
-
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = 'uploads'
+    VALIDATED_FOLDER = 'validated'
+    DIFFERENT_FOLDER = 'different'
+    SECRET_KEY = 'supersecretykey'
